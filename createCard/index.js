ee1e62f93c079fe42cc4e0fbca3977a35c8add96
John Mathis <JohnDMathis@users.noreply.github.com>
2022-09-19 11:33:00 -0500

    test more

--- createCard/index.js
+++ createCard/index.js
@@ -1,6 +1,6 @@
 "use strict";
 warning( "version: 20")
-console.log( "version: 20")
+setOutput( "version", "20");
 const { setOutput, warning } = require( "@actions/core" );
 const leankitApiFactory = require( "../leankit/api" );
 const { getInputParams, reportError, validateLeankitUrl } = require( "../leankit/helpers" );
@@ -18,8 +18,8 @@ const { getInputParams, reportError, validateLeankitUrl } = require( "../leankit
 	validateLeankitUrl( "host", host );
 
 	const { createCard } = leankitApiFactory( host, apiToken );
-	warning( `laneId:${ laneId }`)
-	warning( `typeId:${ typeId }`)
+	setOutput( "laneId", laneId );
+	setOutput( "typeId", typeId );
 	const payload = { boardId, title, laneId, typeId };
 	// if ( laneId ) {
 	// 	payload.laneId = laneId;
